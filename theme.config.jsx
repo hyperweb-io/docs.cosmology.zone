a6c3bfec1862b6e6cf613275959dc4cc17b21cff
liujun93 <ljun8575@gmail.com>
2023-11-29 11:03:17 +0800

    update

---
+++ theme.config.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default {
+  logo: <span>Cosmology Documentation</span>,
+  project: {
+    link: "https://github.com/cosmology-tech/sign",
+  },
+  // ... other theme options
+};
