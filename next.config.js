eb0955149785823da67454c92676477676ab9b85
liujun93 <ljun8575@gmail.com>
2023-11-30 17:01:12 +0800

    deploy

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const withNextra = require("nextra")({
   themeConfig: "./theme.config.jsx",
 });
 
-module.exports = withNextra();
-
-// If you have other Next.js configurations, you can pass them as the parameter:
-// module.exports = withNextra({ /* other next.js config */ })
+module.exports = withNextra({
+  output: 'export',
+  images: {
+    domains: [
+      "user-images.githubusercontent.com",
+      "github.com",
+      "img.shields.io",
+    ],
+    unoptimized: true,
+  },
+});
