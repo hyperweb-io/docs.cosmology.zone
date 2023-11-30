7f1c149ee86fd8f1fe71ae3aedc04b62be152ac7
liujun93 <ljun8575@gmail.com>
2023-11-30 22:54:50 +0800

    add github link

--- theme.config.jsx
+++ theme.config.jsx
@@ -9,13 +9,28 @@ export default {
       width="150px"
     />
   ),
+  logoLink: "https://docs.cosmology.zone",
   project: {
     link: "https://github.com/cosmology-tech",
   },
-  docsRepositoryBase: "https://github.com/cosmology-tech/docs",
   editLink: {
-    text: "Edit this page on GitHub",
+    text: "Edit this page →",
   },
+  feedback: {
+    content: "Go to project Github →",
+    useLink: () => {
+      const { asPath } = useRouter();
+      const head = "https://github.com/cosmology-tech";
+      const project = asPath.split("/")[1];
+      switch (project) {
+        case "ts-codegen":
+          return "https://github.com/CosmWasm/ts-codegen";
+        default:
+          return `${head}/${project}`;
+      }
+    },
+  },
+  docsRepositoryBase: "https://github.com/cosmology-tech/docs/tree/main",
   chat: {
     link: "https://discord.gg/6hy8KQ9aJY",
   },
@@ -23,12 +38,13 @@ export default {
     const { asPath, defaultLocale, locale } = useRouter();
     const { title } = useConfig();
     const url =
-      "https://docs.cosmology.zone/" +
+      "https://docs.cosmology.zone" +
       (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
 
     const _title = asPath !== "/" ? `${title} - Cosmology` : `${title}`;
     return (
       <>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:url" content={url} />
         <meta property="og:title" content={_title} />
         <meta property="og:description" content={"Web3 Products Provider"} />
@@ -39,5 +55,19 @@ export default {
   },
   sidebar: {
     defaultMenuCollapseLevel: 1,
+  },
+  primaryHue: 260,
+  primarySaturation: 75,
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s – Cosmology",
+      };
+    } else {
+      return {
+        titleTemplate: "%s",
+      };
+    }
   },
 };
